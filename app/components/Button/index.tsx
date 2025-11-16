9758c48cf574601b54f943f137d474293db07ab4
Edson <edsonfkz@gmail.com>
2025-11-15 23:30:16 -0500

    Changing button config file and adding disabled function

--- app/components/Button/index.tsx
+++ app/components/Button/index.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 
-import { buttonDefaultCss } from "@/config/components/buttonCss";
+import { buttonDefaultCss } from "@/config/components/button";
 
 interface ButtonProps {
   label: string;
   onClick: () => void;
   className?: string;
+  disabled?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ label, onClick, className }) => {
+const Button: React.FC<ButtonProps> = ({ label, onClick, className, disabled }) => {
   return (
-    <button onClick={onClick} className={className || buttonDefaultCss}>
+    <button onClick={onClick} className={className || buttonDefaultCss} disabled={disabled}>
       {label}
     </button>
   );
